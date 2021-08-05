143236939d74a8e9e71381f588fdcedbdf880d4e
chrisgaulke <chris.gaulke@gmail.com>
2021-08-05 11:04:20 -0500

    adding analyses

--- colon_cancer.R
+++ colon_cancer.R
@@ -27,6 +27,16 @@
 
 # Purpose: Data wrangling and viz
 
+#Questions
+
+
+#1. What are the differences between early onset (<50 years) and late onset (>65)
+#   microbiomes
+#2. Are there microbiome differences in patients who have high CFD,
+#   C7 complement protein expression- this can be divided into two groups
+#   by median expression or quartiles and compare top 25% vs bottom 25%
+#   (regardless of age, other variables)
+
 
 # SET ENVIRONMENT ---------------------------------------------------------
 
@@ -61,6 +71,17 @@ colon_cancer_sample.metadata <- read.table("../coadread_tcga_pan_can_atlas_2018/
                                            header = T,
                                            quote ="",
                                            comment.char = "#")
+
+#gene expression data for C7 and CFD
+cancer_gene.df <- read.table(file = "../coadread_tcga_pan_can_atlas_2018/modified_gene_data/gene_quantification_C7_CFD.txt",
+                             row.names = 1,
+                             sep = "\t",
+                             header = T,
+                             quote ="",
+                             comment.char = "#",
+                             as.is = T)
+
+cancer_gene.df$Entrez_Gene_Id <- NULL #do not want
 
 
 # ANALYSIS: DATA WRANGLE ------------------------------------------------------
@@ -140,13 +161,44 @@ rf.cancer <- t(rf.cancer)
 all(rownames(colon_cancer_patient.metadata) == rownames(rf.cancer))
 
 
-# ANALYSIS: AGE -----------------------------------------------------------
+# ANALYSIS: DATA WRANGLE GENES --------------------------------------------
+
+#start with genes and fix names
+colnames(cancer_gene.df) <-
+  gsub(x = colnames(cancer_gene.df), pattern = "\\.", "-")
+
+#fix colnames again
+colnames(cancer_gene.df) <-
+  gsub(x = colnames(cancer_gene.df), pattern = "-01", "")
+
+cancer_gene.df <- cancer_gene.df[,
+                    which(
+                      colnames(cancer_gene.df) %in%
+                        rownames(colon_cancer_patient.metadata)
+                      )
+                    ]
+
+cancer_gene.df <- cancer_gene.df[,rownames(colon_cancer_patient.metadata)]
+
+cancer_gene.df <- t(cancer_gene.df)
+cancer_gene.df <- as.data.frame(cancer_gene.df)
+
+#add to metadata
+
+colon_cancer_patient.metadata$CFD <- cancer_gene.df$CFD
+colon_cancer_patient.metadata$C7 <- cancer_gene.df$C7
+
+# ANALYSIS: BETA DIVERSITY  ------------------------------------------------------
 
 #Lets looks to see how age of diagnosis is distributed in these data
 #recall from the full metadata file that "AGE" is actually age of diagnosis
 #For now we will assume all of these samples have cancer
 
 hist(colon_cancer_patient.metadata$AGE)
+
+#note that this is equiv to:
+
+hist(colon_cancer_patient.metadata[,"AGE"])
 
 #lets also sweep out some summary stats here
 
@@ -157,29 +209,118 @@ mean(colon_cancer_patient.metadata$AGE)
 fivenum(colon_cancer_patient.metadata$AGE)
 
 #Now we can set up tentative age bins
-#early < 60 ; normal 60 - 70; late > 70
+#early < 50 ; normal 51 - 79; late > 79
 
 colon_cancer_patient.metadata$GROUP <-
-
-  factor(
-
     cut(
-      colon_cancer_patient.metadata$AGE, c(0,60,70,100)
+      colon_cancer_patient.metadata$AGE, c(0,50,65,100)
       )
 
-    )
 
 #now make these numbers more readable
-levels(colon_cancer_patient.metadata$GROUP) <- c("0-60", "61-70", ">70")
+levels(colon_cancer_patient.metadata$GROUP) <- c("0-50", "51-65", ">65")
 
 #cut can be tricky so always check to make sure things are as you want them
 
-View(colon_cancer_patient.metadata[,c("AGE", "GROUP")])
+#View(colon_cancer_patient.metadata[,c("AGE", "GROUP")])
 
 #now lets take a look at how many samples are in each group
 table(colon_cancer_patient.metadata$GROUP)
 
+#lets have a look at how
+
+cancer.prcomp <- prcomp(rf.cancer,scale =F, center = T )
+
+#make a skree plot to identify important dimensions
+plot(cancer.prcomp)
+
+cancer_prcomp.df <- cancer.prcomp$x
+cancer_prcomp.df <- as.data.frame(cancer_prcomp.df)
+cancer_prcomp.df$GROUP <- colon_cancer_patient.metadata$GROUP
+cancer_prcomp.df$C7 <- colon_cancer_patient.metadata$C7
+cancer_prcomp.df$CFD <- colon_cancer_patient.metadata$CFD
+cancer_prcomp.df$CENTER <- colon_cancer_patient.metadata$CENTER
+
+cancer_prcomp_age.plot <- ggplot(cancer_prcomp.df, aes(x = PC1,
+                                                       y = PC2,
+                                                       color = GROUP))
+
+cancer_prcomp_age.plot +
+  geom_point()
+
+# we can color the points by gene expression to get a high level view of
+cancer_prcomp_age.plot <- ggplot(cancer_prcomp.df, aes(x = PC1,
+                                                       y = PC2,
+                                                       color = C7))
+
+cancer_prcomp_age.plot +
+  geom_point()
+
+cancer_prcomp_age.plot <- ggplot(cancer_prcomp.df, aes(x = PC1,
+                                                       y = PC2,
+                                                       color = CFD))
+
+cancer_prcomp_age.plot +
+  geom_point()
+
+#Center
+cancer_prcomp_age.plot <- ggplot(cancer_prcomp.df, aes(x = PC1,
+                                                       y = PC2,
+                                                       color = CENTER))
+
+cancer_prcomp_age.plot +
+  geom_point()
+
+#adonis note because these data are prenormalized we will get a warning here
+#we should approach this result with skepticism.
+cancer_age.adonis <- adonis(rf.cancer ~ colon_cancer_patient.metadata$GROUP, permutations = 1000)
+cancer_age_C7.adonis <- adonis(rf.cancer ~ colon_cancer_patient.metadata$C7, permutations = 1000)
+cancer_age_CFD.adonis <- adonis(rf.cancer ~ colon_cancer_patient.metadata$CFD, permutations = 1000)
+
+cancer_age.adonis #not sig
+cancer_age_C7.adonis #sig
+cancer_age_CFD.adonis #sig
+
+#Since we can't trust adonis but together with the PCA it seems like there may
+# be an association between C7 or CFD and the microbiome we can try to determine
+# if the vector of gene expression values correlate with the dimensions of the
+# ordination. We can do this with a simple cor.test or with envfit. We will try
+# both here
+
+cancer_metadata.envfit <- envfit(cancer.prcomp,
+                        colon_cancer_patient.metadata[,c("GROUP",
+                                                         "C7",
+                                                         "CFD",
+                                                         "CENTER"
+                                                         )],
+                        na.rm = T,
+                        permutations = 5000,choices = c(1,2))
+
+cancer_metadata.envfit
+
+#we can also just use independent correlations with the different PCs we will
+# limit this to the first three dims
+
+cor.test(cancer_prcomp.df$PC1,cancer_prcomp.df$C7, method = "spearman")# n.s
+cor.test(cancer_prcomp.df$PC2,cancer_prcomp.df$C7, method = "spearman")# **
+cor.test(cancer_prcomp.df$PC3,cancer_prcomp.df$C7, method = "spearman")# n.s
+
+cor.test(cancer_prcomp.df$PC1,cancer_prcomp.df$CFD, method = "spearman")# n.s
+cor.test(cancer_prcomp.df$PC2,cancer_prcomp.df$CFD, method = "spearman")# **
+cor.test(cancer_prcomp.df$PC3,cancer_prcomp.df$CFD, method = "spearman")# n.s
+
+
+# ANALYSIS: RANDOM FORESTS -----------------------------------------------
+
+
 #finally lets looks to see how well a classifier does at binning colon cancer
 #patients
 
-randomForest(y = colon_cancer_patient.metadata$GROUP, x = rf.cancer)
+age.rf <- randomForest(y = colon_cancer_patient.metadata$GROUP, x = rf.cancer)
+
+#take a look at performance
+age.rf
+
+#classifier does pretty awful. The OOB error is high and the class error is
+#~1 for two groups. This would suggest that there aren't strong differences
+# between age groups with regard to microbiome composition.
